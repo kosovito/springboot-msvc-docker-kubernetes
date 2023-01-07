f36dfb1fc3097520efcca9678684048d2067a0c1
kosovito <kosovito@gmail.com>
2023-01-07 13:35:46 +0100

    initial commit

---
+++ msvc-usuarios/src/main/java/org/kosovito/springcloud/msvc/usuarios/MsvcUsuariosApplication.java
@@ -0,0 +1,13 @@
+package org.kosovito.springcloud.msvc.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcUsuariosApplication.class, args);
+	}
+
+}
