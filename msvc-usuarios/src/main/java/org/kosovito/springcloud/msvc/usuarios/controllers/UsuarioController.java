99b3d7c9f35acf87c9d5bef7b5fd0a2cfa855045
kosovito <kosovito@gmail.com>
2023-02-18 12:19:29 +0100

    180223 Creando microservicio OAuth2 y configurado el cliente

--- msvc-usuarios/src/main/java/org/kosovito/springcloud/msvc/usuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/org/kosovito/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -107,6 +107,11 @@ public class UsuarioController {
         return ResponseEntity.ok(service.listarPorIds(ids));
     }
 
+    @GetMapping("/authorized")
+    public Map<String, Object> authorized(@RequestParam(name = "code") String code){
+        return Collections.singletonMap("code", code);
+    }
+
     private static ResponseEntity<Map<String, String>> validar(BindingResult result) {
         Map<String, String> errores = new HashMap<>();
         result.getFieldErrors().forEach(err -> {
